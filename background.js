99886e5368d46e76ec8685707da623d7e636e3f6
fazer-git <fazerdev@protonmail.com>
2026-06-25 18:41:42 -0400

    fully working

--- background.js
+++ background.js
@@ -1,19 +1,24 @@
 let rerun = new Set();
 
-chrome.downloads.onCreated.addListener((item) => {
-    const value = chrome.storage.local.get(["key"]);
-    console.log(value.key);
-    if(rerun.has(item.url)) return;
+chrome.downloads.onCreated.addListener(async (item) => {
+    const value = await chrome.storage.local.get(["key"]);
+
+    const key = value.key; 
+
+    if (!key) return;
+
+    if (rerun.has(item.url)) return;
+
     console.log("Download started:", item.url);
 
     chrome.downloads.cancel(item.id, () => {
         rerun.add(item.url);
+
         console.log("Canceled download:", item.url);
+
         chrome.downloads.download({
             url: item.url,
             saveAs: true
         });
     });
-
-
 });
