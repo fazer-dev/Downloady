b2188f6cb6d72df74cd316e848cd17651005df25
fazer-git <fazerdev@protonmail.com>
2026-06-26 09:10:01 -0400

    Small tweaks

--- chrome_src/background.js
+++ chrome_src/background.js
@@ -9,16 +9,20 @@ chrome.downloads.onCreated.addListener(async (item) => {
 
     if (rerun.has(item.url)) return;
 
-    console.log("Download started:", item.url);
+	console.log("Download started:", item.url);
 
-    chrome.downloads.cancel(item.id, () => {
-        rerun.add(item.url);
+	chrome.downloads.cancel(item.id, () => {
+		rerun.add(item.url);
 
-        console.log("Canceled download:", item.url);
+		console.log("Canceled download:", item.url);
 
-        chrome.downloads.download({
-            url: item.url,
-            saveAs: true
-        });
-    });
+		chrome.downloads.download({
+		url: item.url,
+		saveAs: true
+		});
+	
+		console.log("Downloaded and correctly placed url:", item.url);
+
+	});
 });
+
